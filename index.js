e0e0422c26e7868ae0bda18f87d46283af4a8e94
root <root@openshiftmaster.zippyops.com>
2020-01-18 15:19:28 +0530

    new

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+app.get('/',function(req, res){
+	res.sendfile('index.html');
+});
+
+
+app.listen(3000, function () {
+	console.log("Server running at http://0.0.0.0:3000");
+});
